f6e1cb78db3b941e39c7d56e36749cf51eca3c5d
LorenzoFlowFir <46107911+LorenzoFlowFir@users.noreply.github.com>
2023-11-24 14:39:13 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'who_liked_that',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
