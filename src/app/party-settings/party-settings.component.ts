437de5100542be248735c790fc3a41f1f2e64208
Raphael <rschleininger@outlook.fr>
2024-02-22 08:37:30 +0100

    first commit create component party-settings

---
+++ src/app/party-settings/party-settings.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-settings',
+  templateUrl: './party-settings.component.html',
+  styleUrls: ['./party-settings.component.scss'],
+})
+export class PartySettingsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
